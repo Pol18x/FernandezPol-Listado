b83fe1755dfe7c8a259acc68224ce601d4eb9450
doliaalex2004-alt <doliaalex2004@gmail.com>
2025-12-12 19:26:44 +0100

    DoliaAlexander.java

--- ejercicios/DoliaAlexander.java
+++ ejercicios/DoliaAlexander.java
@@ -1,6 +1,6 @@
 public class DoliaAlexander {
     public static void main(String[] args) {
-        System.out.println("Ejercicio entregado por José Luis González Sánchez");
+        System.out.println("Ejercicio entregado por Dolia Alexander");
     }
 }
 
